c9c17056411c436cfc3abc5424d0606fc8ec7365
John Lynn <lynnjonathanp@gmail.com>
2015-12-19 23:49:05 -0800

    Initialize Repository

---
+++ desktop/src/com/silferein/pb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.silferein.pb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.silferein.pb.ProjectPeanutButter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectPeanutButter(), config);
+	}
+}
